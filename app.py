94c26b06e0d2f8bb510f99a8184d9be3f077fcfd
zhaowy1206 <zhaowy1206@gmail.com>
2024-04-15 15:17:35 +0800

    modified:   app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    current_time = datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
+    return f'Hello, World! Current date and time is: {current_time}'
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000)
